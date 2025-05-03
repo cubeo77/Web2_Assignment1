042886a18ae876062a8aefd97ef702db9c3e4ec2
Blair Tate <btate7@my.bcit.ca>
2025-05-03 14:56:40 -0700

    Created a members page, logout ejs, and a database js file. currently working to get the database populating correctly.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const session = require('express-session');
 const port = 8000;
+
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 } // 1 min
+}));
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
@@ -11,6 +24,13 @@ app.use(express.static('public'));
 
 // Routes
 app.get('/', (req, res) => {
+
+    if (req.session.user) {
+        console.log(`User ${req.session.user} is already signed in.`);
+        return res.redirect('/members');
+    }
+    console.log('User is not signed in, rendering index page.');
+
     res.render('index', { title: 'Welcome to my Site!' });
 });
 
@@ -21,6 +41,46 @@ app.get('/form/signup', (req, res) => {
 app.get('/form/signin', (req, res) => {
     res.render('partials/form-signIn');
 });
+
+app.get('/logout', (req, res) => {
+
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Error destroying session:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.clearCookie('connect.sid');
+        console.log('Session destroyed successfully.');
+
+        res.redirect('/');
+    })
+});
+
+app.get('/members', (req, res) => {
+    if (req.session.user) {
+        res.render('members', { title: req.session.user });
+    } else {
+        res.redirect('/');
+    }
+});
+
+
+app.post('/signin', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === 'admin' && password === 'password') {
+        req.session.user = username;
+
+        console.log(`User ${username} signed in successfully.`);
+
+        res.redirect('/members');
+    } else {
+
+        console.log(`Failed sign in attempt for user ${username}.`);
+        res.redirect('/');
+    }
+});
+
 
 
 app.listen(port, () => {
